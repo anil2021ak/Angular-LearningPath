a55e0de22a8d16c46127f90f47811f03b3b59f81
Anil <anil2021ak@gmail.com>
2024-06-26 16:50:56 +0530

    Remove uiCrud/src/app from Git index

---
+++ uiCrud/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    url: 'https://dummy.restapiexample.com',
+    REST_API_SERVER : "http://localhost:3000",
+    fakeJsonUrl: 'https://jsonplaceholder.typicode.com/',
+    frontEndTestUrl: 'https://frontendtest.glitch.me',
+
+}
---
+++ uiCrud/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    url: 'https://dummy.restapiexample.com',
+    REST_API_SERVER : "http://localhost:3000",
+    weatherUrl: "https://api.openweathermap.org/",
+     fakeJsonUrl: 'https://jsonplaceholder.typicode.com',
+    frontEndTestUrl: 'https://frontendtest.glitch.me',
+    weatherApiKey:'97ed86b99fdcf738c7a080e0fa9fde20'
+}
